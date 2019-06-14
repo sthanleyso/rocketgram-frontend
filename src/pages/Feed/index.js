4baeeccc054abf51b18cf2b641cf03bec1259c51
sthanleyso <sthanleys7@gmail.com>
2019-06-14 16:00:01 -0300

    Configurado o socket.io, aplicação funcionando em tempo real

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import io from 'socket.io-client';
 import api from '../../services/api';
 
 import { PostList, UserInfo } from './styles';
@@ -14,14 +15,32 @@ export default class Feed extends Component {
   };
 
   async componentDidMount() {
+    this.registerToSocket();
+
     const response = await api.get('posts');
 
     this.setState({ feed: response.data });
   }
 
+  registerToSocket = () => {
+    const socket = io('http://localhost:3333');
+
+    socket.on('post', newPost => {
+      this.setState({ feed: [newPost, this.state.feed] });
+    });
+
+    socket.on('like', likedPost => {
+      this.setState({
+        feed: this.state.feed.map(post =>
+          post._id === likedPost._id ? likedPost : post
+        )
+      });
+    });
+  };
+
   handleLike = id => {
     api.post(`/posts/${id}/like`);
-  }
+  };
 
   render() {
     const { feed } = this.state;
